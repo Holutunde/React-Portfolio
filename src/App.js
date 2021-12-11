8fc19ef46f9ad0bcb706883456a310dc6948a0f0
Holutunde <Holutunde99@gmail.com>
2021-12-11 01:19:02 +0100

    feature(about): about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from './components/about/About'
 import Launch from './components/launch/launch'
 
 const App = () => {
   return (
     <div className="App">
       <Launch />
+      <About />
     </div>
   )
 }
